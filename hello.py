428f8aabfd432df3f7b0b3dd50b6281493d2dc40
Mark <markxploder@gmail.com>
2016-11-12 02:31:33 +0200

    Gunicorn1

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 def wsgi_application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    resp = environ['QUERY_STRING'].split("&")
     start_response(status, headers)
+    resp = environ['QUERY_STRING'].split("&")
+    resp = [item+"\r\n" for item in resp]
     return [resp]
+
