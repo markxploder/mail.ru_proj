2b2a6e435f93724c14a7fbd728c54cea53283a95
Mark <markxploder@gmail.com>
2016-11-15 19:35:30 +0200

    django

---
+++ ask/qa/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url, patterns
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('qa.views',
+   url(r'^$', 'test'),
+   url(r'^login/.*$', 'test', name='login'),
+   url(r'^signup/.*', 'test', name='signup'),
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
+   url(r'^ask/.*', 'test', name='ask'),
+   url(r'^popular/.*', 'test', name='popular'),
+   url(r'^new/.*', 'test', name='new'),
+)
--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application & 
