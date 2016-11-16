abe8db062a33cddc06070bb862531d295a3e9b8e
Mark <markxploder@gmail.com>
2016-11-16 19:56:01 +0200

    blabla

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -b 0.0.0.0:8080 hello:app
-gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application & 
+sudo gunicorn -b 0.0.0.0:8080 hello:app &
+sudo gunicorn -b 0.0.0.0:8000 /home/box/web/ask ask.wsgi:application & 
